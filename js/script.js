38df56c0e5b2ba41f7e54e71a9c153baaf6650bb
Alexei <103207831+Dubrovschii@users.noreply.github.com>
2023-08-07 16:17:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ document.addEventListener('mousemove', e => {
 		`
 	})
 })
+
+
+const imageElements = document.querySelectorAll('.layers__item[style*="background-image"]');
+const imageCache = {};
+
+imageElements.forEach(element => {
+    const backgroundImageURL = element.style.backgroundImage.match(/url\("(.+)"\)/)[1];
+    const cacheKey = backgroundImageURL;
+
+    if (!imageCache[cacheKey]) {
+        const img = new Image();
+        img.src = backgroundImageURL;
+        img.onload = () => {
+            imageCache[cacheKey] = img;
+        };
+    }
+});
+
+// Позже, при необходимости использовать изображения:
+const layer1Image = imageCache['img/bg.webp'];
+const layer5Image = imageCache['img/5.1.webp'];
+const layer6Image = imageCache['img/layer-6.webp'];
+
+// Теперь вы можете использовать эти изображения, например:
+const layer1Element = document.querySelector('.layer-1');
+layer1Element.style.backgroundImage = `url(${layer1Image.src})`;
+
