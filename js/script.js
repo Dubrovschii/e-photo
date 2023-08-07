ec3ea95a120861ca63d0e16af9c08e98a4a42fc9
Alexei <103207831+Dubrovschii@users.noreply.github.com>
2023-08-07 16:43:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,50 +53,50 @@ document.addEventListener('mousemove', e => {
 
 // loadImages();
 
-const imageCache = {};
+// const imageCache = {};
 
-function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = reject;
-        img.src = url;
-    });
-}
+// function loadImage(url) {
+//     return new Promise((resolve, reject) => {
+//         const img = new Image();
+//         img.onload = () => resolve(img);
+//         img.onerror = reject;
+//         img.src = url;
+//     });
+// }
 
-async function getCachedImage(url) {
-    const cacheKey = `image:${url}`;
+// async function getCachedImage(url) {
+//     const cacheKey = `image:${url}`;
 
-    if (imageCache[cacheKey]) {
-        return imageCache[cacheKey];
-    } else {
-        try {
-            const img = await loadImage(url);
-            imageCache[cacheKey] = img;
-            return img;
-        } catch (error) {
-            console.error(`Failed to load image from ${url}:`, error);
-            return null;
-        }
-    }
-}
+//     if (imageCache[cacheKey]) {
+//         return imageCache[cacheKey];
+//     } else {
+//         try {
+//             const img = await loadImage(url);
+//             imageCache[cacheKey] = img;
+//             return img;
+//         } catch (error) {
+//             console.error(`Failed to load image from ${url}:`, error);
+//             return null;
+//         }
+//     }
+// }
 
-async function loadImages() {
-    const layer1ImageURL = 'img/bg.webp';
-    const layer5ImageURL = 'img/5.1.webp';
-    const layer6ImageURL = 'img/layer-6.webp';
+// async function loadImages() {
+//     const layer1ImageURL = 'img/bg.webp';
+//     const layer5ImageURL = 'img/5.1.webp';
+//     const layer6ImageURL = 'img/layer-6.webp';
 
-    const layer1Image = await getCachedImage(layer1ImageURL);
-    const layer5Image = await getCachedImage(layer5ImageURL);
-    const layer6Image = await getCachedImage(layer6ImageURL);
+//     const layer1Image = await getCachedImage(layer1ImageURL);
+//     const layer5Image = await getCachedImage(layer5ImageURL);
+//     const layer6Image = await getCachedImage(layer6ImageURL);
 
-    if (layer1Image && layer1Image.src) {
-        const layer1Element = document.querySelector('.layer-1');
-        layer1Element.style.backgroundImage = `url(${layer1Image.src})`;
-    }
+//     if (layer1Image && layer1Image.src) {
+//         const layer1Element = document.querySelector('.layer-1');
+//         layer1Element.style.backgroundImage = `url(${layer1Image.src})`;
+//     }
 
-    // Подобным образом можно использовать остальные изображения
-}
+//     // Подобным образом можно использовать остальные изображения
+// }
 
-loadImages();
+// loadImages();
 
