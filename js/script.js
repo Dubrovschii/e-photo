b9ef4f4e108cbec81686a8b13debaf8cc4e7cd39
Alexei <103207831+Dubrovschii@users.noreply.github.com>
2023-08-08 14:55:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ document.addEventListener('mousemove', e => {
 		`
 	})
 })
+//for srcoll-up on color.html !!!!!!
+$("img.lazyload").lazyload();
 
+if ($(window).scrollTop() >= 150) $("#ToTop").fadeIn(800);
+$(window).scroll(function(){
+	if ($(window).scrollTop() <= 150) $("#ToTop").fadeOut(800);
+	else $("#ToTop").fadeIn(800);
+});
+
+if ($(window).scrollTop() <= $(document).height()) $("#OnBottom").fadeIn(800);
+$(window).scroll(function(){
+	if ($(window).scrollTop() >= $(document).height()) $("#OnBottom").fadeOut(800);
+	else $("#OnBottom").fadeIn(800);
+});
+
+$("#ToTop").click(function(){$("html,body").animate({scrollTop:0}, 800)});
+$("#OnBottom").click(function(){$("html,body").animate({scrollTop:$(document).height()}, 800)});	
+
+Fancybox.bind("[data-fancybox]", {
+});
+//for srcoll-up on color.html !!!!!!
 
 const imageCache = {};
 
